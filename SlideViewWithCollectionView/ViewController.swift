e0f591b6dfdba1e494e034ee25d8d29f73a329d6
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-12-23 23:03:47 +0700

    - Initial Project `SlideViewWithCollectionView`.

---
+++ SlideViewWithCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideViewWithCollectionView
+//
+//  Created by Kittitat Rodphotong on 12/23/2559 BE.
+//  Copyright © 2559 DevGo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
